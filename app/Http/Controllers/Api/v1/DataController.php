3c8503dda00f0c64ba1c1120f9deb61a842fb164
Diego Rodrigues <diego.mrodrigues@outlook.com>
2016-07-21 14:48:28 -0300

    Small Fix

--- app/Http/Controllers/Api/v1/DataController.php
+++ app/Http/Controllers/Api/v1/DataController.php
@@ -23,17 +23,12 @@ class DataController extends Controller
     public function send(Request $request)
     {
         $device = Device::find($request->device_id);
-        $pins = Pin::where('active', true)
-                   ->get();
 
         foreach ($request->data as $key => $value)
         {
             $pd = new PinData("device_data_" . $device->id);
 
-            $pd->pin_id = collect($pins)->search(function ($item) use ($value) {
-                return $item->name == $value["name"];
-            })["id"];
-
+            $pd->pin_id = $value["id"];
             $pd->value = $value["value"];
 
             $pd->save();
